689122ec80231daa808f3a5a1ce6d88c61996e83
Benjamin Audurier <benjamin.audurier@cea.fr>
2023-07-20 11:11:32 +0200

    change to G4GeneralParticleSource

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -31,10 +31,12 @@
 #define PrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4GeneralParticleSource.hh"
 #include "G4ParticleGun.hh"
 #include "globals.hh"
 
 class G4ParticleGun;
+class G4GeneralParticleSource;
 class G4Event;
 class G4Box;
 
@@ -56,10 +58,10 @@ class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
     void GeneratePrimaries(G4Event*) override;
 
     // method to access particle gun
-    const G4ParticleGun* GetParticleGun() const { return fParticleGun; }
+    const G4GeneralParticleSource* GetParticleGun() const { return fGPS; }
 
   private:
-    G4ParticleGun* fParticleGun = nullptr; // pointer a to G4 gun class
+    G4GeneralParticleSource* fGPS = nullptr; // pointer a to G4 gun class
     G4Box* fEnvelopeBox = nullptr;
 };
 
